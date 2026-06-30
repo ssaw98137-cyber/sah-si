62db359ec1d51d8068c4a01a1bde939ccf0e277e
ssaw98137-cyber <ssaw98137@gmail.com>
2026-06-30 15:36:26 +0300

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // export const api_route = "http://localhost:8080";
-export const api_route = "https://sahr-se.onrender.com";
+export const api_route = "https://sah-se.onrender.com";
 
 export const socket = io(api_route, { autoConnect: true });
